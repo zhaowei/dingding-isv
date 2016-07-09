fafb133953d402f942f1aee9bd695ca410cecfee
zhaowei <zhaowei.me@gmail.com>
2016-07-09 11:20:51 +0800

    增加字段

--- src/main/java/dingdingisv/web/rest/dto/IsvappPermantCodeDTO.java
+++ src/main/java/dingdingisv/web/rest/dto/IsvappPermantCodeDTO.java
@@ -22,6 +22,37 @@ public class IsvappPermantCodeDTO implements Serializable {
 
     private String jsTicket;
 
+    private String authCorpInfo;
+
+    private String inviteCode;
+
+    private String industry;
+
+    private String corpName;
+
+    private String licenseCode;
+
+    private String isAuthenticated;
+
+    private String inviteUrl;
+
+    private String authUserInfo;
+
+    private String corpLogoUrl;
+
+    private String authInfo;
+
+    private String agent;
+
+    private String agentid;
+
+    private String agentName;
+
+    private String logoUrl;
+
+    private String appid;
+
+
     private ZonedDateTime createTime;
 
     private ZonedDateTime updateTime;
@@ -95,21 +126,6 @@ public class IsvappPermantCodeDTO implements Serializable {
         return Objects.hashCode(id);
     }
 
-    @Override
-    public String toString() {
-        return "IsvappPermantCodeDTO{" +
-            "id=" + id +
-            ", isvFid=" + isvFid +
-            ", corpId='" + corpId + '\'' +
-            ", permantCode='" + permantCode + '\'' +
-            ", accessToken='" + accessToken + '\'' +
-            ", jsTicket='" + jsTicket + '\'' +
-            ", createTime=" + createTime +
-            ", updateTime=" + updateTime +
-            ", beginTime=" + beginTime +
-            '}';
-    }
-
     public String getAccessToken() {
         return accessToken;
     }
@@ -131,7 +147,157 @@ public class IsvappPermantCodeDTO implements Serializable {
     }
 
     public void setBeginTime(ZonedDateTime beginTime) {
+
         this.beginTime = beginTime;
     }
 
+    @Override
+    public String toString() {
+        return "IsvappPermantCodeDTO{" +
+            "id=" + id +
+            ", isvFid=" + isvFid +
+            ", corpId='" + corpId + '\'' +
+            ", permantCode='" + permantCode + '\'' +
+            ", accessToken='" + accessToken + '\'' +
+            ", jsTicket='" + jsTicket + '\'' +
+            ", authCorpInfo='" + authCorpInfo + '\'' +
+            ", inviteCode='" + inviteCode + '\'' +
+            ", industry='" + industry + '\'' +
+            ", corpName='" + corpName + '\'' +
+            ", licenseCode='" + licenseCode + '\'' +
+            ", isAuthenticated='" + isAuthenticated + '\'' +
+            ", inviteUrl='" + inviteUrl + '\'' +
+            ", authUserInfo='" + authUserInfo + '\'' +
+            ", corpLogoUrl='" + corpLogoUrl + '\'' +
+            ", authInfo='" + authInfo + '\'' +
+            ", agent='" + agent + '\'' +
+            ", agentid='" + agentid + '\'' +
+            ", agentName='" + agentName + '\'' +
+            ", logoUrl='" + logoUrl + '\'' +
+            ", appid='" + appid + '\'' +
+            ", createTime=" + createTime +
+            ", updateTime=" + updateTime +
+            ", beginTime=" + beginTime +
+            '}';
+    }
+
+    public String getAuthCorpInfo() {
+        return authCorpInfo;
+    }
+
+    public void setAuthCorpInfo(String authCorpInfo) {
+        this.authCorpInfo = authCorpInfo;
+    }
+
+    public String getInviteCode() {
+        return inviteCode;
+    }
+
+    public void setInviteCode(String inviteCode) {
+        this.inviteCode = inviteCode;
+    }
+
+    public String getIndustry() {
+        return industry;
+    }
+
+    public void setIndustry(String industry) {
+        this.industry = industry;
+    }
+
+    public String getCorpName() {
+        return corpName;
+    }
+
+    public void setCorpName(String corpName) {
+        this.corpName = corpName;
+    }
+
+    public String getLicenseCode() {
+        return licenseCode;
+    }
+
+    public void setLicenseCode(String licenseCode) {
+        this.licenseCode = licenseCode;
+    }
+
+    public String getIsAuthenticated() {
+        return isAuthenticated;
+    }
+
+    public void setIsAuthenticated(String isAuthenticated) {
+        this.isAuthenticated = isAuthenticated;
+    }
+
+    public String getInviteUrl() {
+        return inviteUrl;
+    }
+
+    public void setInviteUrl(String inviteUrl) {
+        this.inviteUrl = inviteUrl;
+    }
+
+    public String getAuthUserInfo() {
+        return authUserInfo;
+    }
+
+    public void setAuthUserInfo(String authUserInfo) {
+        this.authUserInfo = authUserInfo;
+    }
+
+    public String getCorpLogoUrl() {
+        return corpLogoUrl;
+    }
+
+    public void setCorpLogoUrl(String corpLogoUrl) {
+        this.corpLogoUrl = corpLogoUrl;
+    }
+
+    public String getAuthInfo() {
+        return authInfo;
+    }
+
+    public void setAuthInfo(String authInfo) {
+        this.authInfo = authInfo;
+    }
+
+    public String getAgent() {
+        return agent;
+    }
+
+    public void setAgent(String agent) {
+        this.agent = agent;
+    }
+
+    public String getAgentid() {
+        return agentid;
+    }
+
+    public void setAgentid(String agentid) {
+        this.agentid = agentid;
+    }
+
+    public String getAgentName() {
+        return agentName;
+    }
+
+    public void setAgentName(String agentName) {
+        this.agentName = agentName;
+    }
+
+    public String getLogoUrl() {
+        return logoUrl;
+    }
+
+    public void setLogoUrl(String logoUrl) {
+        this.logoUrl = logoUrl;
+    }
+
+    public String getAppid() {
+        return appid;
+    }
+
+    public void setAppid(String appid) {
+        this.appid = appid;
+    }
 }
